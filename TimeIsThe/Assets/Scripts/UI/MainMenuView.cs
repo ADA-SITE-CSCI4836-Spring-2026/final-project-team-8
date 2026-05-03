84261741ee0ce8b8c0e930539ce36db8e4ee6e38
Zaur Rajabov <zradj7@gmail.com>
2026-05-03 17:49:07 +0400

    ui fixes

--- TimeIsThe/Assets/Scripts/UI/MainMenuView.cs
+++ TimeIsThe/Assets/Scripts/UI/MainMenuView.cs
@@ -15,18 +15,18 @@ public class MainMenuView : UIView
 
     private void Awake()
     {
-        playButton?.onClick.AddListener(OnPlayClicked);
-        quitButton?.onClick.AddListener(OnQuitClicked);
-        instructionsButton?.onClick.AddListener(OnInstructionsClicked);
-        backButton?.onClick.AddListener(OnBackClicked);
+        playButton.onClick.AddListener(OnPlayClicked);
+        quitButton.onClick.AddListener(OnQuitClicked);
+        instructionsButton.onClick.AddListener(OnInstructionsClicked);
+        backButton.onClick.AddListener(OnBackClicked);
     }
 
     private void OnDestroy()
     {
-        playButton?.onClick.RemoveListener(OnPlayClicked);
-        quitButton?.onClick.RemoveListener(OnQuitClicked);
-        instructionsButton?.onClick.RemoveListener(OnInstructionsClicked);
-        backButton?.onClick.RemoveListener(OnBackClicked);
+        playButton.onClick.RemoveListener(OnPlayClicked);
+        quitButton.onClick.RemoveListener(OnQuitClicked);
+        instructionsButton.onClick.RemoveListener(OnInstructionsClicked);
+        backButton.onClick.RemoveListener(OnBackClicked);
     }
 
     private void OnPlayClicked()
