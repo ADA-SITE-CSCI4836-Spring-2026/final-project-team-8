c70fd91ac9ec60437c378c33a977b4f59a3b4f91
Emillock <mirzazadeemil2005@gmail.com>
2026-05-03 18:53:07 +0400

    upd scripts

--- TimeIsThe/Assets/Scripts/UI/PauseView.cs
+++ TimeIsThe/Assets/Scripts/UI/PauseView.cs
@@ -38,6 +38,8 @@ public class PauseView : MonoBehaviour
 
     private void OnRestartGameClicked()
     {
+        PauseManager.Resume();
+        SceneLoader.Instance.ReloadCurrentScene();
     }
 
     private void OnMainMenuClicked()
